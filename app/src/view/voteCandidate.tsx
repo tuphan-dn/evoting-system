98df32b5dbfebc036cded4af1d6e4d8ae01548d6
tuphan-dn <tuphan@descartes.network>
2022-04-28 15:41:24 +0700

    update dapp ui

--- app/src/view/voteCandidate.tsx
+++ app/src/view/voteCandidate.tsx
@@ -1,7 +1,7 @@
-import { useSelector, useDispatch } from 'react-redux'
 import { Fragment, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { useConnectedWallet } from '@gokiprotocol/walletkit'
-import * as anchor from '@project-serum/anchor'
+import { web3, utils, BN } from '@project-serum/anchor'
 
 import { Button, Col, Input, Modal, notification, Row, Typography } from 'antd'
 
@@ -22,30 +22,30 @@ const VoteCandidate = ({ candidateAddress }: { candidateAddress: string }) => {
   const onVote = async () => {
     if (!wallet) return
     const program = getProgram(wallet)
-    const candidatePublicKey = new anchor.web3.PublicKey(candidateAddress)
-    const mintPublicKey = new anchor.web3.PublicKey(candidateData.mint)
+    const candidatePublicKey = new web3.PublicKey(candidateAddress)
+    const mintPublicKey = new web3.PublicKey(candidateData.mint)
 
-    const [treasurer] = await anchor.web3.PublicKey.findProgramAddress(
+    const [treasurer] = await web3.PublicKey.findProgramAddress(
       [Buffer.from('treasurer'), candidatePublicKey.toBuffer()],
       program.programId,
     )
-    const [ballot] = await anchor.web3.PublicKey.findProgramAddress(
+    const [ballot] = await web3.PublicKey.findProgramAddress(
       [Buffer.from('ballot'), candidatePublicKey.toBuffer(), wallet.publicKey.toBuffer()],
       program.programId,
     )
     // Derive token account
-    let walletTokenAccount = await anchor.utils.token.associatedAddress({
+    let walletTokenAccount = await utils.token.associatedAddress({
       mint: mintPublicKey,
       owner: wallet.publicKey,
     })
-    let candidateTokenAccount = await anchor.utils.token.associatedAddress({
+    let candidateTokenAccount = await utils.token.associatedAddress({
       mint: mintPublicKey,
       owner: treasurer,
     })
 
     try {
       setLoading(true)
-      await program.rpc.vote(new anchor.BN(amount), {
+      await program.rpc.vote(new BN(amount), {
         accounts: {
           authority: wallet.publicKey,
           candidate: candidatePublicKey,
@@ -54,10 +54,10 @@ const VoteCandidate = ({ candidateAddress }: { candidateAddress: string }) => {
           candidateTokenAccount,
           ballot,
           voterTokenAccount: walletTokenAccount,
-          tokenProgram: anchor.utils.token.TOKEN_PROGRAM_ID,
-          associatedTokenProgram: anchor.utils.token.ASSOCIATED_PROGRAM_ID,
-          systemProgram: anchor.web3.SystemProgram.programId,
-          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          tokenProgram: utils.token.TOKEN_PROGRAM_ID,
+          associatedTokenProgram: utils.token.ASSOCIATED_PROGRAM_ID,
+          systemProgram: web3.SystemProgram.programId,
+          rent: web3.SYSVAR_RENT_PUBKEY,
         },
         signers: [],
       })
