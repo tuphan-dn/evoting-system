9bdc2a65338e6a1b431ba621ccccaa78f1975b3d
tuphan-dn <tuphan@descartes.network>
2022-05-06 09:21:47 +0700

    bugfix

--- app/src/config/idl.ts
+++ app/src/config/idl.ts
@@ -1,503 +1,503 @@
 export type L6 = {
-  "version": "0.1.0",
-  "name": "l6",
-  "instructions": [
+  version: '0.1.0'
+  name: 'l6'
+  instructions: [
     {
-      "name": "initializeCandidate",
-      "accounts": [
+      name: 'initializeCandidate'
+      accounts: [
         {
-          "name": "authority",
-          "isMut": true,
-          "isSigner": true
+          name: 'authority'
+          isMut: true
+          isSigner: true
         },
         {
-          "name": "candidate",
-          "isMut": true,
-          "isSigner": true
+          name: 'candidate'
+          isMut: true
+          isSigner: true
         },
         {
-          "name": "treasurer",
-          "isMut": false,
-          "isSigner": false
+          name: 'treasurer'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "mint",
-          "isMut": false,
-          "isSigner": false
+          name: 'mint'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "candidateTokenAccount",
-          "isMut": true,
-          "isSigner": false
+          name: 'candidateTokenAccount'
+          isMut: true
+          isSigner: false
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'systemProgram'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'tokenProgram'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "associatedTokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'associatedTokenProgram'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": [
+          name: 'rent'
+          isMut: false
+          isSigner: false
+        },
+      ]
+      args: [
         {
-          "name": "startDate",
-          "type": "i64"
+          name: 'startDate'
+          type: 'i64'
         },
         {
-          "name": "endDate",
-          "type": "i64"
-        }
+          name: 'endDate'
+          type: 'i64'
+        },
       ]
     },
     {
-      "name": "vote",
-      "accounts": [
+      name: 'vote'
+      accounts: [
         {
-          "name": "authority",
-          "isMut": true,
-          "isSigner": true
+          name: 'authority'
+          isMut: true
+          isSigner: true
         },
         {
-          "name": "candidate",
-          "isMut": true,
-          "isSigner": false
+          name: 'candidate'
+          isMut: true
+          isSigner: false
         },
         {
-          "name": "treasurer",
-          "isMut": false,
-          "isSigner": false
+          name: 'treasurer'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "mint",
-          "isMut": false,
-          "isSigner": false
+          name: 'mint'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "candidateTokenAccount",
-          "isMut": true,
-          "isSigner": false
+          name: 'candidateTokenAccount'
+          isMut: true
+          isSigner: false
         },
         {
-          "name": "ballot",
-          "isMut": true,
-          "isSigner": false
+          name: 'ballot'
+          isMut: true
+          isSigner: false
         },
         {
-          "name": "voterTokenAccount",
-          "isMut": true,
-          "isSigner": false
+          name: 'voterTokenAccount'
+          isMut: true
+          isSigner: false
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'systemProgram'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'tokenProgram'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "associatedTokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'associatedTokenProgram'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": [
+          name: 'rent'
+          isMut: false
+          isSigner: false
+        },
+      ]
+      args: [
         {
-          "name": "amount",
-          "type": "u64"
-        }
+          name: 'amount'
+          type: 'u64'
+        },
       ]
     },
     {
-      "name": "close",
-      "accounts": [
+      name: 'close'
+      accounts: [
         {
-          "name": "authority",
-          "isMut": true,
-          "isSigner": true
+          name: 'authority'
+          isMut: true
+          isSigner: true
         },
         {
-          "name": "candidate",
-          "isMut": true,
-          "isSigner": false
+          name: 'candidate'
+          isMut: true
+          isSigner: false
         },
         {
-          "name": "treasurer",
-          "isMut": false,
-          "isSigner": false
+          name: 'treasurer'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "mint",
-          "isMut": false,
-          "isSigner": false
+          name: 'mint'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "candidateTokenAccount",
-          "isMut": true,
-          "isSigner": false
+          name: 'candidateTokenAccount'
+          isMut: true
+          isSigner: false
         },
         {
-          "name": "ballot",
-          "isMut": true,
-          "isSigner": false
+          name: 'ballot'
+          isMut: true
+          isSigner: false
         },
         {
-          "name": "voterTokenAccount",
-          "isMut": true,
-          "isSigner": false
+          name: 'voterTokenAccount'
+          isMut: true
+          isSigner: false
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'systemProgram'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'tokenProgram'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "associatedTokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'associatedTokenProgram'
+          isMut: false
+          isSigner: false
         },
         {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        }
-      ],
-      "args": []
-    }
-  ],
-  "accounts": [
-    {
-      "name": "ballot",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "authority",
-            "type": "publicKey"
-          },
-          {
-            "name": "candidate",
-            "type": "publicKey"
-          },
-          {
-            "name": "amount",
-            "type": "u64"
-          }
-        ]
-      }
+          name: 'rent'
+          isMut: false
+          isSigner: false
+        },
+      ]
+      args: []
     },
-    {
-      "name": "candidate",
-      "type": {
-        "kind": "struct",
-        "fields": [
-          {
-            "name": "mint",
-            "type": "publicKey"
-          },
-          {
-            "name": "amount",
-            "type": "u64"
-          },
-          {
-            "name": "startDate",
-            "type": "i64"
-          },
-          {
-            "name": "endDate",
-            "type": "i64"
-          }
-        ]
-      }
-    }
-  ],
-  "errors": [
-    {
-      "code": 6000,
-      "name": "NotActiveCandidate",
-      "msg": "The candidate isn't active"
-    },
-    {
-      "code": 6001,
-      "name": "EndedCandidate",
-      "msg": "The candidate is ended"
-    }
   ]
-};
+  accounts: [
+    {
+      name: 'ballot'
+      type: {
+        kind: 'struct'
+        fields: [
+          {
+            name: 'authority'
+            type: 'publicKey'
+          },
+          {
+            name: 'candidate'
+            type: 'publicKey'
+          },
+          {
+            name: 'amount'
+            type: 'u64'
+          },
+        ]
+      }
+    },
+    {
+      name: 'candidate'
+      type: {
+        kind: 'struct'
+        fields: [
+          {
+            name: 'mint'
+            type: 'publicKey'
+          },
+          {
+            name: 'amount'
+            type: 'u64'
+          },
+          {
+            name: 'startDate'
+            type: 'i64'
+          },
+          {
+            name: 'endDate'
+            type: 'i64'
+          },
+        ]
+      }
+    },
+  ]
+  errors: [
+    {
+      code: 6000
+      name: 'NotActiveCandidate'
+      msg: "The candidate isn't active"
+    },
+    {
+      code: 6001
+      name: 'NotEndedCandidate'
+      msg: "The candidate isn't ended"
+    },
+  ]
+}
 
 export const IDL: L6 = {
-  "version": "0.1.0",
-  "name": "l6",
-  "instructions": [
+  version: '0.1.0',
+  name: 'l6',
+  instructions: [
     {
-      "name": "initializeCandidate",
-      "accounts": [
+      name: 'initializeCandidate',
+      accounts: [
         {
-          "name": "authority",
-          "isMut": true,
-          "isSigner": true
+          name: 'authority',
+          isMut: true,
+          isSigner: true,
         },
         {
-          "name": "candidate",
-          "isMut": true,
-          "isSigner": true
+          name: 'candidate',
+          isMut: true,
+          isSigner: true,
         },
         {
-          "name": "treasurer",
-          "isMut": false,
-          "isSigner": false
+          name: 'treasurer',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "mint",
-          "isMut": false,
-          "isSigner": false
+          name: 'mint',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "candidateTokenAccount",
-          "isMut": true,
-          "isSigner": false
+          name: 'candidateTokenAccount',
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'systemProgram',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'tokenProgram',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "associatedTokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'associatedTokenProgram',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        }
+          name: 'rent',
+          isMut: false,
+          isSigner: false,
+        },
       ],
-      "args": [
+      args: [
         {
-          "name": "startDate",
-          "type": "i64"
+          name: 'startDate',
+          type: 'i64',
         },
         {
-          "name": "endDate",
-          "type": "i64"
-        }
-      ]
+          name: 'endDate',
+          type: 'i64',
+        },
+      ],
     },
     {
-      "name": "vote",
-      "accounts": [
+      name: 'vote',
+      accounts: [
         {
-          "name": "authority",
-          "isMut": true,
-          "isSigner": true
+          name: 'authority',
+          isMut: true,
+          isSigner: true,
         },
         {
-          "name": "candidate",
-          "isMut": true,
-          "isSigner": false
+          name: 'candidate',
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "treasurer",
-          "isMut": false,
-          "isSigner": false
+          name: 'treasurer',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "mint",
-          "isMut": false,
-          "isSigner": false
+          name: 'mint',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "candidateTokenAccount",
-          "isMut": true,
-          "isSigner": false
+          name: 'candidateTokenAccount',
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "ballot",
-          "isMut": true,
-          "isSigner": false
+          name: 'ballot',
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "voterTokenAccount",
-          "isMut": true,
-          "isSigner": false
+          name: 'voterTokenAccount',
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'systemProgram',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'tokenProgram',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "associatedTokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'associatedTokenProgram',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        }
+          name: 'rent',
+          isMut: false,
+          isSigner: false,
+        },
       ],
-      "args": [
+      args: [
         {
-          "name": "amount",
-          "type": "u64"
-        }
-      ]
+          name: 'amount',
+          type: 'u64',
+        },
+      ],
     },
     {
-      "name": "close",
-      "accounts": [
+      name: 'close',
+      accounts: [
         {
-          "name": "authority",
-          "isMut": true,
-          "isSigner": true
+          name: 'authority',
+          isMut: true,
+          isSigner: true,
         },
         {
-          "name": "candidate",
-          "isMut": true,
-          "isSigner": false
+          name: 'candidate',
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "treasurer",
-          "isMut": false,
-          "isSigner": false
+          name: 'treasurer',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "mint",
-          "isMut": false,
-          "isSigner": false
+          name: 'mint',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "candidateTokenAccount",
-          "isMut": true,
-          "isSigner": false
+          name: 'candidateTokenAccount',
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "ballot",
-          "isMut": true,
-          "isSigner": false
+          name: 'ballot',
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "voterTokenAccount",
-          "isMut": true,
-          "isSigner": false
+          name: 'voterTokenAccount',
+          isMut: true,
+          isSigner: false,
         },
         {
-          "name": "systemProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'systemProgram',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "tokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'tokenProgram',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "associatedTokenProgram",
-          "isMut": false,
-          "isSigner": false
+          name: 'associatedTokenProgram',
+          isMut: false,
+          isSigner: false,
         },
         {
-          "name": "rent",
-          "isMut": false,
-          "isSigner": false
-        }
+          name: 'rent',
+          isMut: false,
+          isSigner: false,
+        },
       ],
-      "args": []
-    }
+      args: [],
+    },
   ],
-  "accounts": [
+  accounts: [
     {
-      "name": "ballot",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: 'ballot',
+      type: {
+        kind: 'struct',
+        fields: [
           {
-            "name": "authority",
-            "type": "publicKey"
+            name: 'authority',
+            type: 'publicKey',
           },
           {
-            "name": "candidate",
-            "type": "publicKey"
+            name: 'candidate',
+            type: 'publicKey',
           },
           {
-            "name": "amount",
-            "type": "u64"
-          }
-        ]
-      }
+            name: 'amount',
+            type: 'u64',
+          },
+        ],
+      },
     },
     {
-      "name": "candidate",
-      "type": {
-        "kind": "struct",
-        "fields": [
+      name: 'candidate',
+      type: {
+        kind: 'struct',
+        fields: [
           {
-            "name": "mint",
-            "type": "publicKey"
+            name: 'mint',
+            type: 'publicKey',
           },
           {
-            "name": "amount",
-            "type": "u64"
+            name: 'amount',
+            type: 'u64',
           },
           {
-            "name": "startDate",
-            "type": "i64"
+            name: 'startDate',
+            type: 'i64',
           },
           {
-            "name": "endDate",
-            "type": "i64"
-          }
-        ]
-      }
-    }
+            name: 'endDate',
+            type: 'i64',
+          },
+        ],
+      },
+    },
   ],
-  "errors": [
+  errors: [
     {
-      "code": 6000,
-      "name": "NotActiveCandidate",
-      "msg": "The candidate isn't active"
+      code: 6000,
+      name: 'NotActiveCandidate',
+      msg: "The candidate isn't active",
     },
     {
-      "code": 6001,
-      "name": "EndedCandidate",
-      "msg": "The candidate is ended"
-    }
-  ]
-};
+      code: 6001,
+      name: 'NotEndedCandidate',
+      msg: "The candidate isn't ended",
+    },
+  ],
+}
